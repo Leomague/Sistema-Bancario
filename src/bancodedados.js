a81277ceb30c9daad96f425d3db99a26ee0a20c6
Leomague <ldmagallaes@gmail.com>
2023-08-31 19:06:58 -0300

    implemetado correções em arquivos, varrido banco de dados

--- src/bancodedados.js
+++ src/bancodedados.js
@@ -5,32 +5,8 @@ module.exports = {
         agencia: '0001',
         senha: 'Cubos123Bank'
     },
-    contas:[ {
-        numero:  1,
-        saldo: 100,
-        usuario: {
-            nome: "Foo Bar",
-            cpf: "00011122233",
-            data_nascimento: "2001-03-15",
-            telefone: "11999998888",
-            email: "foo@bar.com",
-            senha: "1234"
-        }
-    }
-],
-    saques: [{
-        numero_conta: "1",
-        valor: 10000,
-        senha: "1234"
-    }],
-    depositos: [{
-        numero_conta : "1",
-        valor : 10000
-    }],
-    transferencias: [{
-        numero_conta_origem: "1",
-        numero_conta_destino: "2",
-        valor: 10000,
-        senha: "1234"
-    }]
+    contas: [],
+    saques: [],
+    depositos: [],
+    transferencias: []
 }
